4feb5393f7a5bdc036a98e03e3cc78fa6e4b0572
nilson98bra <nilson.98@hotmail.com>
2022-01-17 11:06:27 -0300

    leaflet-velocity att

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,66 +1,93 @@
 
 function initDemoMap(){
 
-    var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
-        'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    });
+  var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
+      'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  });
 
-    var Esri_DarkGreyCanvas = L.tileLayer(
-        "http://{s}.sm.mapstack.stamen.com/" +
-        "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
-        "{z}/{x}/{y}.png",
-        {
-            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
-            'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-        }
-    );
+  var Esri_DarkGreyCanvas = L.tileLayer(
+      "http://{s}.sm.mapstack.stamen.com/" +
+      "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
+      "{z}/{x}/{y}.png",
+      {
+          attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
+          'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+      }
+  );
 
-    var baseLayers = {
-        "Satellite": Esri_WorldImagery,
-        "Grey Canvas": Esri_DarkGreyCanvas
-    };
+  var baseLayers = {
+      "Satellite": Esri_WorldImagery,
+      "Grey Canvas": Esri_DarkGreyCanvas
+  };
 
-    var map = L.map('map', {
-        layers: [ Esri_WorldImagery ]
-    });
+  var map = L.map('map', {
+      layers: [ Esri_WorldImagery ]
+  });
 
-    var layerControl = L.control.layers(baseLayers);
-    layerControl.addTo(map);
-    map.setView([50.00, 14.44], 3);
+  var layerControl = L.control.layers(baseLayers);
+  layerControl.addTo(map);
+  map.setView([-22, 150], 5);
 
-    return {
-        map: map,
-        layerControl: layerControl
-    };
+  return {
+      map: map,
+      layerControl: layerControl
+  };
 }
 
 // demo map
 var mapStuff = initDemoMap();
 var map = mapStuff.map;
 var layerControl = mapStuff.layerControl;
-var handleError = function(err){
-    console.log('handleError...');
-    console.log(err);
-};
 
-WindJSLeaflet.init({
-	localMode: true,
-	map: map,
-	layerControl: layerControl,
-	useNearest: false,
-	timeISO: null,
-	nearestDaysLimit: 7,
-	displayValues: true,
-	displayOptions: {
-		displayPosition: 'bottomright',
-		displayEmptyString: 'No wind data'
-	},
-	overlayName: 'wind',
+// load data (u, v grids) from somewhere (e.g. https://github.com/danwild/wind-js-server)
+$.getJSON('/js/wind-gbr.json', function (data) {
 
-	// https://github.com/danwild/wind-js-server
-	pingUrl: 'http://localhost:7000/alive',
-	latestUrl: 'http://localhost:7000/latest',
-	nearestUrl: 'http://localhost:7000/nearest',
-	errorCallback: handleError
+var velocityLayer = L.velocityLayer({
+  displayValues: true,
+  displayOptions: {
+    velocityType: 'GBR Wind',
+    displayPosition: 'bottomleft',
+    displayEmptyString: 'No wind data'
+  },
+  data: data,
+  maxVelocity: 10
 });
+
+layerControl.addOverlay(velocityLayer, 'Wind - Great Barrier Reef');
+});
+
+$.getJSON('/js/water-gbr.json', function (data) {
+
+  var velocityLayer = L.velocityLayer({
+    displayValues: true,
+    displayOptions: {
+      velocityType: 'GBR Water',
+      displayPosition: 'bottomleft',
+      displayEmptyString: 'No water data'
+    },
+    data: data,
+    maxVelocity: 0.6,
+    velocityScale: 0.10
+  });
+
+  layerControl.addOverlay(velocityLayer, 'Ocean Current - Great Barrier Reef');
+});
+
+$.getJSON('/js/wind-global.json', function (data) {
+
+var velocityLayer = L.velocityLayer({
+  displayValues: true,
+  displayOptions: {
+    velocityType: 'Global Wind',
+    displayPosition: 'bottomleft',
+    displayEmptyString: 'No wind data'
+  },
+  data: data,
+  maxVelocity: 15
+});
+
+layerControl.addOverlay(velocityLayer, 'Wind - Global');
+});
+
+
